8139fbd0880862157256465437762c87b06fb388
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2023-02-20 13:04:22 -0600

    se añadió un ejemplo con el dataset forge y el knneighbors

--- ej1.py
+++ ej1.py
@@ -4,6 +4,8 @@ import pandas as pd
 from sklearn.datasets import load_breast_cancer
 from sklearn.datasets import load_boston
 import mglearn
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 # breast cancer
 cancer=load_breast_cancer()
@@ -22,4 +24,12 @@ X, y = mglearn.datasets.load_extended_boston() # joblib==1.1.0
 print("X.shape: {}".format(X.shape))
 
 mglearn.plots.plot_knn_classification(n_neighbors=4) # se usa el forge
-plt.show()
+# plt.show()
+
+# knn en forge con 3 vecinos
+X, y = mglearn.datasets.make_forge()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+clf = KNeighborsClassifier(n_neighbors=3)
+clf.fit(X_train, y_train)
+print("Test set predictions: {}".format(clf.predict(X_test))) # se ve que tan bien se entrenó
+print("Test set accuracy: {:.2f}".format(clf.score(X_test, y_test))) # se ve el score
