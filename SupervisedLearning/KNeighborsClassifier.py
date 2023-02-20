deed8b97e1d2861bfabdf62ac8c5886865a1f465
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2023-02-20 13:34:56 -0600

    Se creó la carpeta de Supervised Learning

---
+++ SupervisedLearning/KNeighborsClassifier.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_boston
+import mglearn
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
