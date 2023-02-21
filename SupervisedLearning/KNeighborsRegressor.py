d7faf024876aa90b5586775cf0765679b9b92091
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2023-02-21 15:50:44 -0600

    se añadió el kn regressor

---
+++ SupervisedLearning/KNeighborsRegressor.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import mglearn
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+
+X, y = mglearn.datasets.make_wave(n_samples=40)
+# split the wave dataset into a training and a test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+# instantiate the model and set the number of neighbors to consider to 3
+reg = KNeighborsRegressor(n_neighbors=3)
+# fit the model using the training data and training targets
+reg.fit(X_train, y_train)
